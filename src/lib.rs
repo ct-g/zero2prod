a5c099e2a876a1a93f3fb20a65ae0a80fb86e6ba
Conor Tumbers <conor.tumbers@gmail.com>
2023-04-11 16:45:34 +1000

    up to 3.7

--- src/lib.rs
+++ src/lib.rs
@@ -1,16 +1,19 @@
 use axum::{
+    Form,
     http::StatusCode, // Re-export of http crate
-    response::IntoResponse,
     Router,
-    routing::{get, IntoMakeService},
+    routing::{get, post, IntoMakeService},
     Server // Re-export of Server from hyper crate
 };
+
+use serde::Deserialize;
 
 use std::net::TcpListener;
 
 pub fn run(listener: TcpListener) -> Result<Server<hyper::server::conn::AddrIncoming, IntoMakeService<Router>>, hyper::Error> {
     let app = Router::new()
-        .route("/health_check", get(health_check));
+        .route("/health_check", get(health_check))
+        .route("/subscriptions", post(subscribe));
 
     let server = Server::from_tcp(listener)?
         .serve(app.into_make_service());
@@ -18,6 +21,16 @@ pub fn run(listener: TcpListener) -> Result<Server<hyper::server::conn::AddrInco
     Ok(server)
 }
 
-async fn health_check() -> impl IntoResponse{
+#[derive(Deserialize)]
+struct FormData {
+    email: String,
+    name: String,
+}
+
+async fn health_check() -> StatusCode {
+    StatusCode::OK
+}
+
+async fn subscribe(_form: Form<FormData>) -> StatusCode {
     StatusCode::OK
 }
--- tests/health_check.rs
+++ tests/health_check.rs
@@ -1,9 +1,20 @@
 use std::net::TcpListener;
 
+fn spawn_app() -> String {
+    let listener = TcpListener::bind("127.0.0.1:0")
+        .expect("Failed to bind random port");
+    let port = listener.local_addr().unwrap().port();
+    let server = zero2prod::run(listener).expect("Failed to bind address");
+    // Spawn server as a background task
+    let _ = tokio::spawn(server);
+
+    format!("http://127.0.0.1:{}", port)
+}
+
 #[tokio::test]
 async fn health_check_works() {
     // Arrange
-    let address = spawn_app().await;
+    let address = spawn_app();
 
     let client = reqwest::Client::new();
 
@@ -18,13 +29,55 @@ async fn health_check_works() {
     assert_eq!(Some(0), response.content_length());
 }
 
-async fn spawn_app() -> String {
-    let listener = TcpListener::bind("127.0.0.1:0")
-        .expect("Failed to bind random port");
-    let port = listener.local_addr().unwrap().port();
-    let server = zero2prod::run(listener).expect("Failed to bind address");
-    // Spawn server as a background task
-    let _ = tokio::spawn(server);
+#[tokio::test]
+async fn subscribe_and_return_a_200_for_valid_form_data() {
+    // Arrange
+    let app_address = spawn_app();
+    let client = reqwest::Client::new();
 
-    format!("http://127.0.0.1:{}", port)
+    // Act
+    let body = "name=le%20guin&email=ursula_le_guin%40gmail.com";
+    let response = client.post(&format!("{}/subscriptions", &app_address))
+        .header("Content-Type", "application/x-www-form-urlencoded")
+        .body(body)
+        .send()
+        .await
+        .expect("Failed to execute request.");
+
+    // Assert
+    assert_eq!(200, response.status().as_u16());
+}
+
+#[tokio::test]
+// In zero2prod, this test uses 400 with actix_web
+// https://docs.rs/axum/latest/src/axum/extract/rejection.rs.html#73
+// Axum appears to use 422 for the same type of failure
+// The case where the form fails on the body content
+async fn subscribe_returns_a_422_when_data_is_missing() {
+    // Arrange
+    let app_address = spawn_app();
+    let client = reqwest::Client::new();
+    let test_cases = vec![
+        ("name=le%20guin", "missing the email"),
+        ("email=ursula_le_guin%40gmail.com", "missing the name"),
+        ("", "missing everything")
+    ];
+
+    for (invalid_body, error_message) in test_cases {
+        // Act
+        let response = client.post(&format!("{}/subscriptions", &app_address))
+            .header("Content-Type", "application/x-www-form-urlencoded")
+            .body(invalid_body)
+            .send()
+            .await
+            .expect("Failed to execute request");
+
+        // Assert
+        assert_eq!(
+            422,
+            response.status().as_u16(),
+            "The API did not fail with 400 bad request when the payload was {}",
+            error_message
+        )
+    }
 }
