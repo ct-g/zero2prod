b7e23d6fa09a976b3d4d44afc52f79bdf8c0c5ef
Conor Tumbers <conor.tumbers@gmail.com>
2023-06-15 13:31:02 +1000

    Up to 5.4

--- src/main.rs
+++ src/main.rs
@@ -5,7 +5,8 @@ use zero2prod::telemetry::{get_subscriber, init_subscriber};
 use std::net::TcpListener;
 
 use secrecy::ExposeSecret;
-use sqlx::PgPool;
+
+use sqlx::postgres::PgPoolOptions;
 
 #[tokio::main]
 async fn main() -> Result<(), hyper::Error> {
@@ -16,13 +17,14 @@ async fn main() -> Result<(), hyper::Error> {
     // Setup server
     let configuration = get_configuration().expect("Failed to read configuration");
 
-    let connection_pool = PgPool::connect(
+    let connection_pool = PgPoolOptions::new()
+        .acquire_timeout(std::time::Duration::from_secs(2))
+        .connect_lazy(
         &configuration.database.connection_string().expose_secret()
     )
-    .await
     .expect("Failed to connect to Postgres");
 
-    let address = format!("127.0.0.1:{}", configuration.application_port);
+    let address = format!("{}:{}", configuration.application.host, configuration.application.port);
     let listener = TcpListener::bind(address).expect("Unable to bind to address");
     run(listener, connection_pool)?.await
 }
