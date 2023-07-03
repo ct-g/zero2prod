84c6197f462f207ab392d2a64dc524b071c165b5
Conor Tumbers <conor.tumbers@gmail.com>
2023-07-03 14:13:05 +1000

    Up to 7.3.9

--- src/startup.rs
+++ src/startup.rs
@@ -1,7 +1,9 @@
+use crate::configuration::{DatabaseSettings, Settings};
 use crate::email_client::EmailClient;
 use crate::routes::{health_check, subscribe};
 
 use sqlx::PgPool;
+use sqlx::postgres::PgPoolOptions;
 
 use axum::{
     Extension,
@@ -11,6 +13,54 @@ use axum::{
 };
 
 use std::{net::TcpListener, sync::Arc};
+
+pub struct Application {
+    port: u16,
+    server: Server<hyper::server::conn::AddrIncoming, IntoMakeService<Router>>,
+}
+
+impl Application {
+    pub async fn build(
+        configuration: Settings
+    ) -> Result<Self, hyper::Error> {
+        let connection_pool = get_connection_pool(&configuration.database); 
+    
+        let sender_email = configuration
+            .email_client
+            .sender()
+            .expect("Invalid sender email address");
+        let timeout = configuration.email_client.timeout();
+        let email_client = EmailClient::new(
+            configuration.email_client.base_url,
+            sender_email,
+            configuration.email_client.authorisation_token,
+            timeout,
+        );
+    
+        let address = format!("{}:{}", configuration.application.host, configuration.application.port);
+        let listener = TcpListener::bind(address).expect("Unable to bind to address");
+        let port = listener.local_addr().unwrap().port();
+        let server = run(listener, connection_pool, email_client)?;
+
+        Ok(Self { port, server })
+    }
+
+    pub fn port(&self) -> u16 {
+        self.port
+    }
+
+    pub async fn run_until_stopped(self) -> Result<(), hyper::Error> {
+        self.server.await
+    }
+}
+
+pub fn get_connection_pool(configuration: &DatabaseSettings) -> PgPool {
+    PgPoolOptions::new()
+    .acquire_timeout(std::time::Duration::from_secs(2))
+    .connect_lazy_with(
+        configuration.with_db()
+    )
+}
 
 pub fn run(
     listener: TcpListener,
---
+++ tests/api/subscriptions.rs
@@ -0,0 +1,94 @@
+use crate::helpers::spawn_app;
+
+#[tokio::test]
+async fn subscribe_and_return_a_200_for_valid_form_data() {
+    // Arrange
+    let test_app = spawn_app().await;
+    let client = reqwest::Client::new();
+
+    // Act
+    let body = "name=le%20guin&email=ursula_le_guin%40gmail.com";
+    let response = client.post(&format!("{}/subscriptions", &test_app.address))
+        .header("Content-Type", "application/x-www-form-urlencoded")
+        .body(body)
+        .send()
+        .await
+        .expect("Failed to execute request.");
+
+    // Assert
+    assert_eq!(200, response.status().as_u16());
+
+    let saved = sqlx::query!("SELECT email, name FROM subscriptions",)
+        .fetch_one(&test_app.db_pool)
+        .await
+        .expect("Failed to fetch saved subscription");
+
+    assert_eq!(saved.email, "ursula_le_guin@gmail.com");
+    assert_eq!(saved.name, "le guin");
+}
+
+#[tokio::test]
+async fn subscribe_returns_a_400_when_fields_are_present_but_empty() {
+    // Arrange
+    let app = spawn_app().await;
+    let client = reqwest::Client::new();
+    let test_cases = vec![
+        ("name=&email=ursula_le_guin%40gmail.com", "empty name"),
+        ("name=Ursula&email=", "empty email"),
+        ("name=Ursula&email=definitely-not-an-email", "invalid email"),
+    ];
+
+    for (body, description) in test_cases {
+        // Act
+        let response = client
+            .post(&format!("{}/subscriptions", &app.address))
+            .header("Content-Type", "application/x-www-form-urlencoded")
+            .body(body)
+            .send()
+            .await
+            .expect("Failed to execute request");
+
+        // Assert
+        assert_eq!(
+            400,
+            response.status().as_u16(),
+            "The API did not return a 400 Bad Request when the payload was {}",
+            description
+        );
+    }
+}
+
+#[tokio::test]
+// In zero2prod, this test uses 400 with actix_web
+// https://docs.rs/axum/latest/src/axum/extract/rejection.rs.html#73
+// Axum appears to use 422 for the same type of failure
+// The case where the form fails on the body content
+async fn subscribe_returns_a_422_when_data_is_missing() {
+    // Arrange
+    let test_app = spawn_app().await;
+    let client = reqwest::Client::new();
+    let test_cases = vec![
+        ("name=le%20guin", "missing the email"),
+        ("email=ursula_le_guin%40gmail.com", "missing the name"),
+        ("", "missing everything")
+    ];
+
+    for (invalid_body, error_message) in test_cases {
+        // Act
+        let response = client
+            .post(&format!("{}/subscriptions", &test_app.address))
+            .header("Content-Type", "application/x-www-form-urlencoded")
+            .body(invalid_body)
+            .send()
+            .await
+            .expect("Failed to execute request");
+
+        // Assert
+        assert_eq!(
+            422,
+            response.status().as_u16(),
+            "The API did not fail with 422 Unprocessable Content when the payload was {}",
+            error_message
+        )
+    }
+}
